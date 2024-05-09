1f4cfe44735ebd404b1411857f2bcfd5ac509b01
Sourabh Jaiswal <124076587+Sourabhjaiswal12016@users.noreply.github.com>
2024-05-09 19:46:40 +0530

    added comments

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const freecurrencyapi = new Freecurrencyapi(
   "fca_live_d9kE1pc3VkWDqoSSzP6ng4AFDhcNlnmZCmGatB3l"
 );
 
-// convertCurrency("INR", "USD", 3);
+// convertCurrency: fromCurrency To toCurrency:
+// This is very easy npm package to convert  from one currency to another currency:
+//Enjoy the package: made by Sourabh Jaiswal :-
 
 export async function convertCurrency(fromCurrency, toCurrency, units) {
   const res = await freecurrencyapi.latest({
